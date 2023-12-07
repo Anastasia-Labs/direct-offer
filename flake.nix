4af14c5317086d484f2f121b61efedefbc73220e
nikhils9 <nikhilshetty447@gmail.com>
2023-12-07 19:26:45 +0530

    build(flake.nix): add hydraJobs back

--- flake.nix
+++ flake.nix
@@ -71,9 +71,9 @@
           ci.required = [ "all_onchain" ];
         };
 
-      # flake.hydraJobs.x86_64-linux = (
-      #   self.checks.x86_64-linux
-      #   // self.packages.x86_64-linux
-      # );
+      flake.hydraJobs.x86_64-linux = (
+        self.checks.x86_64-linux
+        // self.packages.x86_64-linux
+      );
     };
 }
